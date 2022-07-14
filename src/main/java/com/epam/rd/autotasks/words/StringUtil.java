70fc131e7123c4c5c0ae1c0da83cb8f759d7d23d
Pavel Kulikov <ppkulikov@gmail.com>
2022-07-14 17:31:45 +0300

    Solved. The conditions of the task are not clear (

--- src/main/java/com/epam/rd/autotasks/words/StringUtil.java
+++ src/main/java/com/epam/rd/autotasks/words/StringUtil.java
@@ -1,52 +1,95 @@
 package com.epam.rd.autotasks.words;
 
-import java.util.Arrays;
+
+import java.util.regex.Pattern;
 
 public class StringUtil {
     public static int countEqualIgnoreCaseAndSpaces(String[] words, String sample) {
-        throw new UnsupportedOperationException();
+        if(sample != null && sample != "" && words != null ){
+            int i = 0;
+            for (String word:words){
+                    if (word.strip().compareToIgnoreCase(sample.strip()) == 0) i++;            }
+            return i;} else return 0;
     }
 
     public static String[] splitWords(String text) {
-        throw new UnsupportedOperationException();
+        if (text != null){
+        text = text.replaceFirst("^[ ,.;:?!]+", "");
+        Pattern patForSplit = Pattern.compile("[ ,.;:?!]+");
+        String [] result =  patForSplit.split(text);
+        if (result[0] == "") return null; else return result;} else return null;
+    }
+    static Boolean pathHasError(String path) {
+        Pattern rightPath = Pattern.compile("^[~/(C:)a-zA-Z.]");
+        if (rightPath.matcher(path).find())
+          if (path.indexOf("/") == -1 || path.indexOf("\\") == -1) { //file don't has symbols /&\ at same time
+                if (path.indexOf("C:", 2) == -1 && path.indexOf("~", 1) == -1) { //Symbols C: & ~ don't have in the middle
+                    if (!path.matches("C:(.*)~|~(.*)\\\\(.*)|C:(.*)/"))
+                        if (!path.matches("(.*)/{2}(.*)"))
+                        return false;
+                }
+          //  }
+        }
+        return true;
+    }
+
+    static String convertUnixWin (String path){
+        if(path.indexOf("~") == 0) {
+            path = path.replaceFirst("~","C:\\\\User");
+        } else path = path.replaceFirst("^(/)","C:\\\\");
+        path = path.replace("/","\\");
+        return path;
+    }
+
+    static String convertWinUnix(String path){
+        path = path.replace("C:\\User","~");
+        path = path.replace("C:\\","/");
+        path = path.replace("\\","/");
+        return path;
     }
 
     public static String convertPath(String path, boolean toWin) {
-        throw new UnsupportedOperationException();
+        if(path != null && !path.isEmpty() && !pathHasError(path)){
+        if (path.matches("[/~](.*)") || path.matches("(.*)/(.*)")){
+            if (toWin){
+                return convertUnixWin(path);
+            } else return path;
+        }else {
+            if (!toWin){
+                return convertWinUnix(path);
+            } else return path;
+        }
+
+        } else return null;
     }
 
     public static String joinWords(String[] words) {
-        throw new UnsupportedOperationException();
+        StringBuilder joinWords = new StringBuilder("[");
+        if (words != null && words.length > 0 ){
+            for (String word:words) if (word != ""){
+                joinWords.append(word).append(", ");
+            }
+            if (joinWords.length() > 1) {
+                joinWords.delete(joinWords.length()-2, joinWords.length());
+                joinWords.append("]");} else return null;
+            return String.valueOf(joinWords);} else return null;
     }
 
     public static void main(String[] args) {
-        System.out.println("Test 1: countEqualIgnoreCaseAndSpaces");
-        String[] words = new String[]{" WordS    \t", "words", "w0rds", "WOR  DS", };
-        String sample = "words   ";
+        String unixPath2 = "~\\folder";
+        String convertResult2 = convertPath(unixPath2, false);
+        System.out.println("Result: " + convertResult2);
+        String unixPath3 = "~";
+        String convertResult3 = convertPath(unixPath3, true);
+        System.out.println("Result: " + convertResult3);
+
+
+        String[] words = new String[]{"sch ool", "SCHOO L", "\tschool\t", "     schoo", "s c h o o l"};
+
+        String sample = "scHooL";
         int countResult = countEqualIgnoreCaseAndSpaces(words, sample);
         System.out.println("Result: " + countResult);
-        int expectedCount = 2;
-        System.out.println("Must be: " + expectedCount);
 
-        System.out.println("Test 2: splitWords");
-        String text = "   ,, first, second!!!! third";
-        String[] splitResult = splitWords(text);
-        System.out.println("Result : " + Arrays.toString(splitResult));
-        String[] expectedSplit = new String[]{"first", "second", "third"};
-        System.out.println("Must be: " + Arrays.toString(expectedSplit));
 
-        System.out.println("Test 3: convertPath");
-        String unixPath = "/some/unix/path";
-        String convertResult = convertPath(unixPath, true);
-        System.out.println("Result: " + convertResult);
-        String expectedWinPath = "C:\\some\\unix\\path";
-        System.out.println("Must be: " + expectedWinPath);
-
-        System.out.println("Test 4: joinWords");
-        String[] toJoin = new String[]{"go", "with", "the", "", "FLOW"};
-        String joinResult = joinWords(toJoin);
-        System.out.println("Result: " + joinResult);
-        String expectedJoin = "[go, with, the, FLOW]";
-        System.out.println("Must be: " + expectedJoin);
     }
 }
